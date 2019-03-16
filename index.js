914dfed890a1fa31ee33d903fa36259a91c9ae6a
Kevin Mendez Arce <kevinmenar@gmail.com>
2019-03-16 15:44:05 -0600

    Add firebase instance

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './js/app/App.js';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
